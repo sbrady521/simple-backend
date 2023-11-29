24d47427a629abb343706a554e5aa97a28f95059
sean <sean.brady@stylearcade.com.au>
2023-11-30 08:12:46 +1100

    Setup retool hit

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const fetch = require('node-fetch')
 
-app.use(express.json());
+const app = express()
+const port = process.env.PORT || 3000
+require('dotenv').config()
+
+app.use(express.json())
 
 app.get('/get-retool-url', async (req, res) => {
-    // Your logic to interact with Retool and send back the authenticated URL
-    // Use environment variables for sensitive data like access tokens
+  const url = 'https://app.pathweaver.ai/api/embed-url/external-user'
+  const data = {
+    // Your POST data here as per https://docs.retool.com/apps/external/quickstarts/embed
+    "landingPageUuid": "aa8ca8d4-8cdd-11ee-94a9-37f4d911b569",
+    "groupIds": "2276747",
+    "externalIdentifier": "brent@pathweaver.ai",
+    "userInfo": {
+      "email": "brent@pathweaver.ai"
+    }
+  }
 
-    res.json({ message: "Replace this with the actual response from Retool" });
-});
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.RETOOL_ACCESS_TOKEN}`
+    },
+    body: JSON.stringify(data)
+  })
+
+  const result = await response.join()
+  console.log(result)
+
+
+  res.json({ message: "Replace this with the actual response from Retool" })
+})
 
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+  console.log(`Server listening at http://localhost:${port}`)
+})
